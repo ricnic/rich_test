4c03499c4a43c4411aee3bfd3f339e00155f29fb
Richard <richard.nicolson@gmail.com>
2010-11-20 13:15:53 +0000

    Finished Demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RichTest::Application.config.secret_token = '3e95f55df35c846f5e1732191441adeb470422a9511088ac73219bc4123cced2da04f9443fb17ddd4277eca7f202a99d838fe2ebc0c84bc01905aba70d628641'
